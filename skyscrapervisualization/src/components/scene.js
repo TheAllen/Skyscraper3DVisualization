85dd84bfda919b7c86bf7aa549db51fdb7be12fe
Allen Li <allenlee1995@hotmail.com>
2019-08-07 21:38:36 -0600

    creating a web scene

---
+++ skyscrapervisualization/src/components/scene.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class SceneNav extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            open: false,
+            mouse: false
+        }
+    }
+
+    mouseEnter = () => {
+
+    }
+
+    render() {
+        
+    }
+}
