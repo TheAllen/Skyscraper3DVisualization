33b373a1996063b2cf2ff0f7e6734393f75d29d2
Allen Li <allenlee1995@hotmail.com>
2019-08-11 13:59:23 -0600

    added Paris sceneview

--- skyscrapervisualization/src/components/SceneViewNewYork.js
+++ skyscrapervisualization/src/components/SceneViewNewYork.js
@@ -1,16 +1,22 @@
-import React, {Component} from 'react';
-import {SceneView, Scene} from 'react-sceneview';
+import React, { Component } from 'react';
+import { SceneView, Scene } from 'react-sceneview';
 
 const ID = '34859cee6739438d93262a5aa91bf834'
-const ID1 = 'ae53cf192181425ab999e8a19e41a6dc'
+const ID1 = '0614ea1f9dd043e9ba157b9c20d3c538'
 
 class SceneViewNewYork extends Component {
 
     render() {
-        return(
+        return (
             <SceneView id = "sceneview">
-                <Scene portalItem={{id: ID}}></Scene>
+                <Scene portalItem={{id: ID1}}></Scene>
             </SceneView>
+            // <SceneView id="sceneview">
+            //     <Scene>
+            //         <Layer id="buildings" layerType="scene" url={SCENE_LAYER_URL} />
+            //         <Layer id="districts" layerType="feature" url={FEATURE_LAYER_URL} />
+            //     </Scene>
+            // </SceneView>
         )
     }
 }
