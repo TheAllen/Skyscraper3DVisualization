1cfe66fa42c30e34b80fd31f59905c78578016d4
Allen Li <allenlee1995@hotmail.com>
2019-08-11 14:10:51 -0600

    added 3d visualization for New York

--- skyscrapervisualization/src/components/Constants.js
+++ skyscrapervisualization/src/components/Constants.js
@@ -1,2 +1,4 @@
 
 export const API_KEY = '7d4eb3b5f14128caba7e0a6cfd5bb443';
+
+export const PARISID = ""
--- skyscrapervisualization/src/components/SceneViewNewYork.js
+++ skyscrapervisualization/src/components/SceneViewNewYork.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import { SceneView, Scene } from 'react-sceneview';
 
 const ID = '34859cee6739438d93262a5aa91bf834'
-const ID1 = '0614ea1f9dd043e9ba157b9c20d3c538'
+const IDNY ='d869fa255fd44726ae6e40264e290df1'
+const IDNY1 = '4ea1f7566623413d8ada3b4a6b87c896' //New York
+const ID1 = '0614ea1f9dd043e9ba157b9c20d3c538' //Paris
+const ID2 = 'affa021c51944b5694132b2d61fe1057'
 
 class SceneViewNewYork extends Component {
 
     render() {
         return (
             <SceneView id = "sceneview">
-                <Scene portalItem={{id: ID1}}></Scene>
+                <Scene portalItem={{id: IDNY1}}></Scene>
             </SceneView>
             // <SceneView id="sceneview">
             //     <Scene>
