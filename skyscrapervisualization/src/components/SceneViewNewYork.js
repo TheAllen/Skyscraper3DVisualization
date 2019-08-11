bf162f94c2ae3d314610fdce1dae3c0af1b01e3f
Allen Li <allenlee1995@hotmail.com>
2019-08-11 13:18:15 -0600

    minor update

--- skyscrapervisualization/src/components/SceneViewNewYork.js
+++ skyscrapervisualization/src/components/SceneViewNewYork.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import {SceneView, Scene} from 'react-sceneview';
 
+const ID = '34859cee6739438d93262a5aa91bf834'
+const ID1 = 'ae53cf192181425ab999e8a19e41a6dc'
+
 class SceneViewNewYork extends Component {
 
     render() {
         return(
             <SceneView id = "sceneview">
-                <Scene portalItem={{id: '34859cee6739438d93262a5aa91bf834'}}></Scene>
+                <Scene portalItem={{id: ID}}></Scene>
             </SceneView>
         )
     }
