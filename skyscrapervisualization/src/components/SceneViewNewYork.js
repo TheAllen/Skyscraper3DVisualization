bf32cabc050a0e9cb64ae9a8e844a86313850052
Allen Li <allenlee1995@hotmail.com>
2019-08-14 20:24:24 -0600

    fixed new york visualization

--- skyscrapervisualization/src/components/SceneViewNewYork.js
+++ skyscrapervisualization/src/components/SceneViewNewYork.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { SceneView, Scene, Layer } from 'react-sceneview';
+import { SceneView, Scene, Layer, S } from 'react-sceneview';
 
-const ID = '9a542f6755274436985617a462ffdf44'
-const IDNY ='d869fa255fd44726ae6e40264e290df1'
-const IDNY1 = '4ea1f7566623413d8ada3b4a6b87c896' //New York
+
+const ID = 'a0f52995be7d42c1ba9a2b570570feb5' //Original NY
+const IDNY ='9a542f6755274436985617a462ffdf44'
+const NY_EXAMPLE = 'd50983f1314141bfb6d57c6f83f2690e'
+const IDNY1 = '6da5b4a4f0214f83963bd3620bea376d' //New York
 const ID_PARIS = '0614ea1f9dd043e9ba157b9c20d3c538' //Paris
 const ID_GENEVA = 'c6f90b19164c4283884361005faea852' 
 const ID_LUCERNE = '6043821c854345e4b6d56c9b0765079d'
@@ -14,7 +16,7 @@ class SceneViewNewYork extends Component {
     render() {
         return (
             <SceneView id = "newyork">
-                <Scene portalItem={{id: ID}}>   
+                <Scene portalItem={{id: IDNY1}}>   
                     <Layer id="building" layerType="scene" url={"8be4db26e0784d919923fd91f6cc9882"}></Layer>
 
                 </Scene>
--- skyscrapervisualization/src/components/mainMap.js
+++ skyscrapervisualization/src/components/mainMap.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 import L from 'leaflet';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Map, Marker, Popup, TileLayer, Circle, CircleMarker, Polygon, Polyline, Rectangle } from 'react-leaflet'
-import { Card, Button, CardTitle, CardText } from 'reactstrap';
+import { Card, Button, CardTitle, CardText, Navbar, NavbarBrand, Collapse, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownItem, DropdownMenu, NavbarToggler, DropdownToggle } from 'reactstrap';
+
 
 
 import '../App.css'
@@ -102,6 +103,37 @@ class MainMap extends Component {
 
         return (
             <div className="App">
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand className="navbar-brand mx-auto" href="/">City Visualization</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Options
+                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Option 1
+                  </DropdownItem>
+                                    <DropdownItem>
+                                        Option 2
+                  </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        Reset
+                  </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
                 <Map className='map' center={position} zoom={this.state.zoom}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
