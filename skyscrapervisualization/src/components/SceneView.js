4c37817051821eb18c7acd743babc8d759d6f46f
Allen Li <allenlee1995@hotmail.com>
2019-09-22 14:35:24 -0600

    made the sceneview component reusable

---
+++ skyscrapervisualization/src/components/SceneView.js
@@ -0,0 +1,212 @@
+import React, {Component} from 'react';
+import { SceneView, Scene, Layer, S } from 'react-sceneview';
+import { Card, Button, CardText, CardTitle, Form, FormGroup, Label, Input } from 'reactstrap';
+
+import axios from "axios";
+
+export default class SceneViewCity extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            cityIdentifier: "New York",
+            cityMessage: "New York is my favorite City",
+            id: 1,
+            name: "One World Trade Center",
+            city: "New York City",
+            state: "New York",
+            address: null,
+            buildingDescription: null,
+            height: 0,
+            floors: 0,
+            funFacts: ""
+        }
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange = (e) => {
+
+        this.setState({ [e.target.name]: e.target.value })
+        console.log(e.target.name + ":" + e.target.value);
+    }
+
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        const newBuilding = {
+            cityIdentifier: "New York",
+            cityMessage: "New York is my favorite City(Edit 1)",
+            name: "One World Trade Center",
+            city: "New York City",
+            state: "New York",
+            address: null,
+            buildingDescription: null,
+            height: 1792,
+            floors: 104,
+            funFacts: "One World Trade Center is the main building of the rebuilt World Trade Center complex in Lower Manhattan, New York City"
+        }
+
+        const inputBuilding = {
+            cityIdentifier: this.state.cityIdentifier,
+            cityMessage: this.state.cityMessage,
+            name: this.state.name,
+            city: this.state.city,
+            state: this.state.state,
+            address: null,
+            buildingDescription: this.state.buildingDescription,
+            height: this.state.height,
+            floors: this.state.floors,
+            funFacts: this.state.cityMessage
+        }
+
+        console.log(inputBuilding);
+
+        axios.post('http://localhost:8080/api/skyscraper/', inputBuilding)
+            .then(res => {
+                console.log(res);
+                this.history.push("/");
+            })
+            .catch(error => {
+                console.log(error.response)
+            })
+    }
+
+    render() {
+        return(
+            <div className="App">
+                <SceneView id="newyork">
+                    <Scene portalItem={{ id: this.props.id }}>
+
+                    </Scene>
+                </SceneView>
+
+                <Card body className="city-message-form">
+                    <CardTitle>A 3D View of Manhattan! </CardTitle>
+
+                    {/* Form */}
+
+                    <Form >
+                        <FormGroup className="cityMessage">
+                            {/* <Label for="exampleText">Name</Label> */}
+                            <Input
+                                name="name"
+                                id="exampleText"
+                                placeholder="Enter a name of a building"
+                                onChange={this.onChange}
+                            />
+                        </FormGroup>
+                    </Form>
+
+                    <Form >
+                        <FormGroup className="cityMessage">
+                            <Input
+                                name="city"
+                                id="exampleText"
+                                placeholder="City"
+                                onChange={this.onChange}
+                            />
+                        </FormGroup>
+                    </Form>
+
+                    <Form >
+                        <FormGroup className="cityMessage">
+                            <Input
+                                name="state"
+                                id="exampleText"
+                                placeholder="State"
+                                onChange={this.onChange}
+                            />
+                        </FormGroup>
+                    </Form>
+
+                    <Form >
+                        <FormGroup className="cityMessage">
+
+                            <Input
+                                name="rating"
+                                id="exampleText"
+                                placeholder="Rating"
+                                onChange={this.onChange}
+                            />
+                        </FormGroup>
+                    </Form>
+
+                    <Form >
+                        <FormGroup className="cityMessage">
+
+                            <Input
+                                name="height"
+                                id="exampleText"
+                                placeholder="height"
+                                onChange={this.onChange}
+                            />
+                        </FormGroup>
+                    </Form>
+
+                    <Form >
+                        <FormGroup className="cityMessage">
+
+                            <Input
+                                name="floors"
+                                id="exampleText"
+                                placeholder="floors"
+                                onChange={this.onChange}
+                            />
+                        </FormGroup>
+                    </Form>
+
+                    {/* Fun fact */}
+                    <Form >
+                        <FormGroup className="cityMessage">
+
+                            <Input
+                                type="textarea"
+                                name="buildingDescription"
+                                id="exampleText"
+                                placeholder="Enter a description of the city"
+                                onChange={this.onChange}
+                            />
+                        </FormGroup>
+                    </Form>
+
+                    <Form >
+                        <FormGroup className="cityMessage">
+
+                            <Input
+                                name="cityIdentifier"
+                                id="exampleText"
+                                placeholder="City Identifier"
+                                onChange={this.onChange}
+                            />
+                        </FormGroup>
+                    </Form>
+
+
+                    <Form >
+                        <FormGroup className="cityMessage">
+
+                            <Input
+                                type="textarea"
+                                name="cityMessage"
+                                id="exampleText"
+                                placeholder="City message"
+                                onChange={this.onChange}
+                            />
+                        </FormGroup>
+                    </Form>
+
+                    <Button onClick={this.onSubmit}>Submit</Button>
+                </Card>
+
+                <Card body className="weather-card">
+                    <CardTitle>Weather of New York! </CardTitle>
+                    <CardText>What's your favorite part of the city?</CardText>
+                </Card>
+
+            </div>
+        )
+    }
+}
--- skyscrapervisualization/src/components/SceneViewNewYork.js
+++ skyscrapervisualization/src/components/SceneViewNewYork.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { SceneView, Scene, Layer, S } from 'react-sceneview';
 import { Card, Button, CardText, CardTitle, Form, FormGroup, Label, Input } from 'reactstrap';
-
+import SceneViewCity from './SceneView';
 import { createMessage } from '../Actions/messageAction';
 
 import axios from "axios";
@@ -21,207 +21,209 @@ const ID_BOSTON = '1483a59c30e14d1d8640e6c52133296f'
 
 export class SceneViewNewYork extends Component {
 
-    constructor() {
-        super();
+    // constructor() {
+    //     super();
 
-        this.state = {
-            cityIdentifier: "New York",
-            cityMessage: "New York is my favorite City",
-            id: 1,
-            name: "One World Trade Center",
-            city: "New York City",
-            state: "New York",
-            address: null,
-            buildingDescription: null,
-            height: 0,
-            floors: 0,
-            funFacts: ""
-        }
+    //     this.state = {
+    //         cityIdentifier: "New York",
+    //         cityMessage: "New York is my favorite City",
+    //         id: 1,
+    //         name: "One World Trade Center",
+    //         city: "New York City",
+    //         state: "New York",
+    //         address: null,
+    //         buildingDescription: null,
+    //         height: 0,
+    //         floors: 0,
+    //         funFacts: ""
+    //     }
 
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
-    }
+    //     this.onSubmit = this.onSubmit.bind(this);
+    //     this.onChange = this.onChange.bind(this);
+    // }
 
-    onChange = (e) => {
+    // onChange = (e) => {
 
-        this.setState({ [e.target.name]: e.target.value })
-        console.log(e.target.name + ":" + e.target.value);
-    }
+    //     this.setState({ [e.target.name]: e.target.value })
+    //     console.log(e.target.name + ":" + e.target.value);
+    // }
 
 
-    onSubmit = (e) => {
-        e.preventDefault();
-        const newBuilding = {
-            cityIdentifier: "New York",
-            cityMessage: "New York is my favorite City(Edit 1)",
-            name: "One World Trade Center",
-            city: "New York City",
-            state: "New York",
-            address: null,
-            buildingDescription: null,
-            height: 1792,
-            floors: 104,
-            funFacts: "One World Trade Center is the main building of the rebuilt World Trade Center complex in Lower Manhattan, New York City"
-        }
+    // onSubmit = (e) => {
+    //     e.preventDefault();
+    //     const newBuilding = {
+    //         cityIdentifier: "New York",
+    //         cityMessage: "New York is my favorite City(Edit 1)",
+    //         name: "One World Trade Center",
+    //         city: "New York City",
+    //         state: "New York",
+    //         address: null,
+    //         buildingDescription: null,
+    //         height: 1792,
+    //         floors: 104,
+    //         funFacts: "One World Trade Center is the main building of the rebuilt World Trade Center complex in Lower Manhattan, New York City"
+    //     }
 
-        const inputBuilding = {
-            cityIdentifier: this.state.cityIdentifier,
-            cityMessage: this.state.cityMessage,
-            name: this.state.name,
-            city: this.state.city,
-            state: this.state.state,
-            address: null,
-            buildingDescription: this.state.buildingDescription,
-            height: this.state.height,
-            floors: this.state.floors,
-            funFacts: this.state.cityMessage
-        }
+    //     const inputBuilding = {
+    //         cityIdentifier: this.state.cityIdentifier,
+    //         cityMessage: this.state.cityMessage,
+    //         name: this.state.name,
+    //         city: this.state.city,
+    //         state: this.state.state,
+    //         address: null,
+    //         buildingDescription: this.state.buildingDescription,
+    //         height: this.state.height,
+    //         floors: this.state.floors,
+    //         funFacts: this.state.cityMessage
+    //     }
 
-        console.log(inputBuilding);
+    //     console.log(inputBuilding);
 
-        axios.post('http://localhost:8080/api/skyscraper/', inputBuilding)
-            .then(res => {
-                console.log(res);
-                this.history.push("/");
-            })
-            .catch(error => {
-                console.log(error.response)
-            })
-    }
+    //     axios.post('http://localhost:8080/api/skyscraper/', inputBuilding)
+    //         .then(res => {
+    //             console.log(res);
+    //             this.history.push("/");
+    //         })
+    //         .catch(error => {
+    //             console.log(error.response)
+    //         })
+    // }
 
     render() {
         return (
-            <div className="App">
-                <SceneView id="newyork">
-                    <Scene portalItem={{ id: IDNY }}>
+            // <div className="App">
+            //     <SceneView id="newyork">
+            //         <Scene portalItem={{ id: IDNY }}>
 
-                    </Scene>
-                </SceneView>
+            //         </Scene>
+            //     </SceneView>
 
-                <Card body className="city-message-form">
-                    <CardTitle>A 3D View of Manhattan! </CardTitle>
+            //     <Card body className="city-message-form">
+            //         <CardTitle>A 3D View of Manhattan! </CardTitle>
 
-                    {/* Form */}
+            //         {/* Form */}
 
-                    <Form >
-                        <FormGroup className="cityMessage">
-                            {/* <Label for="exampleText">Name</Label> */}
-                            <Input
-                                name="name"
-                                id="exampleText"
-                                placeholder="Enter a name of a building"
-                                onChange={this.onChange}
-                            />
-                        </FormGroup>
-                    </Form>
+            //         <Form >
+            //             <FormGroup className="cityMessage">
+            //                 {/* <Label for="exampleText">Name</Label> */}
+            //                 <Input
+            //                     name="name"
+            //                     id="exampleText"
+            //                     placeholder="Enter a name of a building"
+            //                     onChange={this.onChange}
+            //                 />
+            //             </FormGroup>
+            //         </Form>
 
-                    <Form >
-                        <FormGroup className="cityMessage">
-                            <Input
-                                name="city"
-                                id="exampleText"
-                                placeholder="City"
-                                onChange={this.onChange}
-                            />
-                        </FormGroup>
-                    </Form>
+            //         <Form >
+            //             <FormGroup className="cityMessage">
+            //                 <Input
+            //                     name="city"
+            //                     id="exampleText"
+            //                     placeholder="City"
+            //                     onChange={this.onChange}
+            //                 />
+            //             </FormGroup>
+            //         </Form>
 
-                    <Form >
-                        <FormGroup className="cityMessage">
-                            <Input
-                                name="state"
-                                id="exampleText"
-                                placeholder="State"
-                                onChange={this.onChange}
-                            />
-                        </FormGroup>
-                    </Form>
+            //         <Form >
+            //             <FormGroup className="cityMessage">
+            //                 <Input
+            //                     name="state"
+            //                     id="exampleText"
+            //                     placeholder="State"
+            //                     onChange={this.onChange}
+            //                 />
+            //             </FormGroup>
+            //         </Form>
 
-                    <Form >
-                        <FormGroup className="cityMessage">
+            //         <Form >
+            //             <FormGroup className="cityMessage">
 
-                            <Input
-                                name="rating"
-                                id="exampleText"
-                                placeholder="Rating"
-                                onChange={this.onChange}
-                            />
-                        </FormGroup>
-                    </Form>
+            //                 <Input
+            //                     name="rating"
+            //                     id="exampleText"
+            //                     placeholder="Rating"
+            //                     onChange={this.onChange}
+            //                 />
+            //             </FormGroup>
+            //         </Form>
 
-                    <Form >
-                        <FormGroup className="cityMessage">
+            //         <Form >
+            //             <FormGroup className="cityMessage">
 
-                            <Input
-                                name="height"
-                                id="exampleText"
-                                placeholder="height"
-                                onChange={this.onChange}
-                            />
-                        </FormGroup>
-                    </Form>
+            //                 <Input
+            //                     name="height"
+            //                     id="exampleText"
+            //                     placeholder="height"
+            //                     onChange={this.onChange}
+            //                 />
+            //             </FormGroup>
+            //         </Form>
 
-                    <Form >
-                        <FormGroup className="cityMessage">
+            //         <Form >
+            //             <FormGroup className="cityMessage">
 
-                            <Input
-                                name="floors"
-                                id="exampleText"
-                                placeholder="floors"
-                                onChange={this.onChange}
-                            />
-                        </FormGroup>
-                    </Form>
+            //                 <Input
+            //                     name="floors"
+            //                     id="exampleText"
+            //                     placeholder="floors"
+            //                     onChange={this.onChange}
+            //                 />
+            //             </FormGroup>
+            //         </Form>
 
-                    {/* Fun fact */}
-                    <Form >
-                        <FormGroup className="cityMessage">
+            //         {/* Fun fact */}
+            //         <Form >
+            //             <FormGroup className="cityMessage">
 
-                            <Input
-                                type="textarea"
-                                name="buildingDescription"
-                                id="exampleText"
-                                placeholder="Enter a description of the city"
-                                onChange={this.onChange}
-                            />
-                        </FormGroup>
-                    </Form>
+            //                 <Input
+            //                     type="textarea"
+            //                     name="buildingDescription"
+            //                     id="exampleText"
+            //                     placeholder="Enter a description of the city"
+            //                     onChange={this.onChange}
+            //                 />
+            //             </FormGroup>
+            //         </Form>
 
-                    <Form >
-                        <FormGroup className="cityMessage">
+            //         <Form >
+            //             <FormGroup className="cityMessage">
 
-                            <Input
-                                name="cityIdentifier"
-                                id="exampleText"
-                                placeholder="City Identifier"
-                                onChange={this.onChange}
-                            />
-                        </FormGroup>
-                    </Form>
+            //                 <Input
+            //                     name="cityIdentifier"
+            //                     id="exampleText"
+            //                     placeholder="City Identifier"
+            //                     onChange={this.onChange}
+            //                 />
+            //             </FormGroup>
+            //         </Form>
 
 
-                    <Form >
-                        <FormGroup className="cityMessage">
+            //         <Form >
+            //             <FormGroup className="cityMessage">
 
-                            <Input
-                                type="textarea"
-                                name="cityMessage"
-                                id="exampleText"
-                                placeholder="City message"
-                                onChange={this.onChange}
-                            />
-                        </FormGroup>
-                    </Form>
+            //                 <Input
+            //                     type="textarea"
+            //                     name="cityMessage"
+            //                     id="exampleText"
+            //                     placeholder="City message"
+            //                     onChange={this.onChange}
+            //                 />
+            //             </FormGroup>
+            //         </Form>
 
-                    <Button onClick={this.onSubmit}>Submit</Button>
-                </Card>
+            //         <Button onClick={this.onSubmit}>Submit</Button>
+            //     </Card>
 
-                <Card body className="weather-card">
-                    <CardTitle>Weather of New York! </CardTitle>
-                    <CardText>What's your favorite part of the city?</CardText>
-                </Card>
+            //     <Card body className="weather-card">
+            //         <CardTitle>Weather of New York! </CardTitle>
+            //         <CardText>What's your favorite part of the city?</CardText>
+            //     </Card>
 
-            </div>
+            // </div>
+
+            <SceneViewCity id={IDNY} />
 
         )
     }
